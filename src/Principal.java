53d10770c2385999d59bf585598ddc2b81c05632
Javier Esteban <javier.gmztjd@gmail.com>
2021-07-16 03:40:55 -0500

    PersonaPOOcreado

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona javier = new Persona();
+		javier.name = "Javier Esteban";
+		javier.age = 24;
+		System.out.println(javier.saludar());
+		System.out.println(javier.miEdad());
+	}
+}
